4998a4c01e14dedfb393665d029b8ba846937f32
Darabos Benjamin <darabosbeni@gmail.com>
2025-04-07 18:48:56 +0200

    Upload pictures to readme, localhost pictures

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
 # faction-panel
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
+### Pictures about the project:
+
+![alt text](image.png)
+![alt text](image-1.png)
+![alt text](image-2.png)
+![alt text](image-3.png)
+![alt text](image-4.png)
+![alt text](image-6.png)
+
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/Storage/CanBeOrderCard.vue
+++ src/components/Storage/CanBeOrderCard.vue
@@ -1,106 +1,111 @@
 <template>
-    <div class="card" >
-        <div style="width: 10%;">
-            <img :src="require(`/imgs/${name}.png`)"  alt="">
-        </div>
-        <div style="width: 25%;">
-            <p class="nameTitle">{{label}}</p>
-        </div>
-        <div style="width: 25%;">
-            <p class="amount">{{price}}$</p>
-        </div>
-        <div style="width: 25%;">
-            <button @click="buyItem()" type="button">Megrendelés</button>
-        </div>
+  <div class="card">
+    <div style="width: 10%">
+      <img
+        src="https://brandlogos.net/wp-content/uploads/2021/11/git-logo.png"
+        alt=""
+      />
     </div>
+    <div style="width: 25%">
+      <p class="nameTitle">{{ label }}</p>
+    </div>
+    <div style="width: 25%">
+      <p class="amount">{{ price }}$</p>
+    </div>
+    <div style="width: 25%">
+      <button @click="buyItem()" type="button">Megrendelés</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CanBeOrderCard',
-    props: {
-        label: String,
-        name: String,
-        price: Number
+  name: "CanBeOrderCard",
+  props: {
+    label: String,
+    name: String,
+    price: Number,
+  },
+  methods: {
+    buyItem() {
+      const itemIndex = this.$store.state.exitsStorage.findIndex(
+        (el) => el.name == this.name
+      );
+      if (itemIndex != -1) {
+        this.$store.state.exitsStorage[itemIndex].count += 1;
+      } else {
+        this.$store.state.exitsStorage.push({
+          name: this.name,
+          count: 1,
+        });
+      }
+      fetch("https://demir_faction/buyItem", {
+        method: "POST",
+        body: JSON.stringify({
+          price: this.price,
+          name: this.name,
+        }),
+      });
     },
-    methods: {
-        buyItem() {
-            const itemIndex = this.$store.state.exitsStorage.findIndex(el => el.name == this.name);
-            if (itemIndex != -1) {
-                this.$store.state.exitsStorage[itemIndex].count += 1;
-            } else {
-                this.$store.state.exitsStorage.push(
-                    {
-                        name: this.name,
-                        count: 1
-                    }
-                )
-            }
-            fetch('https://demir_faction/buyItem', {
-                method: 'POST',
-                body: JSON.stringify({
-                    price: this.price,
-                    name: this.name
-                })
-            })
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 .card {
-    color: white;
-    width: 90%;
-    height: 5vmin;
-    background-color: #1B1A1F;
-    border-radius: 1.5vmin;
-    background: linear-gradient(217deg, #EC786C, rgba(255,0,0,0) 60.71%), linear-gradient(127deg, #1B1A1F, rgba(0,255,0,0) 70.71%), linear-gradient(336deg, #1B1A1F, rgba(0, 0, 0, 0) 70.71%);
-    border-right: 0.3vmin #EC786C solid;
-    border-top: 0.3vmin #EC786C solid;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    padding: 1vmin;
-    margin: 1vmin auto;
+  color: white;
+  width: 90%;
+  height: 5vmin;
+  background-color: #1b1a1f;
+  border-radius: 1.5vmin;
+  background: linear-gradient(217deg, #ec786c, rgba(255, 0, 0, 0) 60.71%),
+    linear-gradient(127deg, #1b1a1f, rgba(0, 255, 0, 0) 70.71%),
+    linear-gradient(336deg, #1b1a1f, rgba(0, 0, 0, 0) 70.71%);
+  border-right: 0.3vmin #ec786c solid;
+  border-top: 0.3vmin #ec786c solid;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 1vmin;
+  margin: 1vmin auto;
 }
 .card img {
-    width: 5vmin;
-    height: 5vmin;
+  width: 5vmin;
+  height: 5vmin;
 }
 .card p {
-    margin: 0;
+  margin: 0;
 }
 .controlContainer {
-    margin-top: 1vmin;
-    width: 50%;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
+  margin-top: 1vmin;
+  width: 50%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
 }
 .nameTitle {
-    font-size: 1.8vmin;
-    letter-spacing: 0.1vmin;
+  font-size: 1.8vmin;
+  letter-spacing: 0.1vmin;
 }
 .amount {
-    font-weight: 700;
-    font-size: 1.6vmin;
-    letter-spacing: 0.1vmin;
+  font-weight: 700;
+  font-size: 1.6vmin;
+  letter-spacing: 0.1vmin;
 }
 button {
-    text-align: center;
-    width: 12vmin;
-    background-color: rgb(39, 39, 39);
-    border: 0.1vmin solid #000;
-    color: white;
-    border-radius: 0.5vmin;
-    display: inline-block;
-    height: 3vmin;
-    font-weight: 700;
-    font-size: 1.5vmin;
+  text-align: center;
+  width: 12vmin;
+  background-color: rgb(39, 39, 39);
+  border: 0.1vmin solid #000;
+  color: white;
+  border-radius: 0.5vmin;
+  display: inline-block;
+  height: 3vmin;
+  font-weight: 700;
+  font-size: 1.5vmin;
 }
 button:hover {
-    cursor: pointer;
-    background-color: rgba(39, 39, 39, 0.692);;
+  cursor: pointer;
+  background-color: rgba(39, 39, 39, 0.692);
 }
 </style>
